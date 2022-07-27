026132072a55119f9e29704f59978c097f59687f
dangolbeeker <dangolbeeker>
2022-07-27 16:39:53 -0500

    routes scaffold styled-components react-router dev-branch

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from 'react';
+import {Routes, Route, } from 'react-router-dom';
+//import About from '../components/about';
+//import Team from '../components/team';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+        <Route path='/about' element={<About />} />
+        <Route path='/team' element={<Team />} />
+        <Route path='/pitch' element={<Pitch />} />
+        <Route path='/roadmap' element={<Roadmap />} />
+    </Routes>
 }
-
-export default App;
